27cb4722753b177aa27bdff60a870cf29ba19868
Paige Drake <pdrake@redhat.com>
2014-06-19 14:51:32 -0700

    return json

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'json'
 require 'sinatra'
+
+get '/' do
+  "the time where this server lives is #{Time.now}
+<br /><br />check out your <a href=\"/agent\">user_agent</a>"
+end
+
+get '/agent' do
+  "you're using #{request.user_agent}"
+end
  
 get '/example.json' do
   content_type :json
