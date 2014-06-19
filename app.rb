4bf36e3d9eabc8c868618281920f002359246ae5
Paige Drake <pdrake@redhat.com>
2014-06-19 12:23:58 -0700

    DB Return JSON

--- app.rb
+++ app.rb
@@ -1,10 +1,36 @@
 require 'sinatra'
+require 'mongo'
+require 'json'
 
 get '/' do
-  "the time where this server lives is #{Time.now}
-    <br /><br />check out your <a href=\"/agent\">user_agent</a>"
+  "handle route and write to db"
 end
 
-get '/agent' do
-  "you're using #{request.user_agent}"
+get '/db' do
+  db = Mongo::Connection.new.db("test")
+  coll = db.collection("foo")
+  doc = {"tester" => "tested", "info" => {"x" => 203, "y" => '102'}}
+  coll.insert(doc)
+  'store'
+end
+
+get '/example.json' do
+  content_type :json
+  { :key1 => 'value1', :key2 => 'value2' }.to_json
+end
+
+
+get '/save/:topVal/:leftVal/:elemId' do
+
+db = Mongo::Connection.new.db("test")
+coll = db.collection("foo")
+topVal = params[:topVal]
+leftVal = params[:leftVal]
+elemId = params[:elemId]
+doc = {"_id"=>elemId, "top"=> topVal, "left'"=>leftVal}
+coll.save(doc)
+
+  content_type :json
+  { :top => params[:topVal], :left => params[:leftVal] }.to_json
+  
 end
